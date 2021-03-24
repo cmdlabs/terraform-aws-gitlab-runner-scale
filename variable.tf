60e2e3e820d5a9540028348be12d295f975f0be8
Mahendra Aryal <maryal@192-168-1-9.tpgi.com.au>
2021-03-25 08:40:47 +1100

    Fixes issue #2

--- variable.tf
+++ variable.tf
@@ -5,36 +5,36 @@ variable "vpc_id" {
 variable "ssh_access" {
   type = object({
     source_cidr = string
-    key_name = string
+    key_name    = string
   })
 }
 
 variable "associate_public_ip_address" {
-  type = bool
+  type    = bool
   default = false
 }
 
 variable "gitlab" {
   type = object({
-    uri = string
-    api_token_ssm_path = string
-    runner_agents_per_instance = number
+    uri                                = string
+    api_token_ssm_path                 = string
+    runner_agents_per_instance         = number
     runner_registration_token_ssm_path = string
-    narrow_to_membership = string
-    log_level = string
+    narrow_to_membership               = string
+    log_level                          = string
   })
 }
 
 variable "asg" {
   type = object({
-    subnet_ids = list(string)
-    min_size = number
-    max_size = number
+    subnet_ids       = list(string)
+    min_size         = number
+    max_size         = number
     desired_capacity = number
   })
 }
 
 variable "lambda_runtime" {
-  type = string
-  default = python3.8
+  type    = string
+  default = "python3.8"
 }
